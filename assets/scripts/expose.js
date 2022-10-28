182b9e796eb795f5d1a17694a0c9d88be9a18ae5
Maximillian Chen <maxchensemail@gmail.com>
2022-10-28 13:15:58 -0700

    drop down menu loads Picture + right audio

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -1,7 +1,17 @@
 // expose.js
 
 window.addEventListener('DOMContentLoaded', init);
+const jsConfetti = new JSConfetti();
 
 function init() {
-  // TODO
+  const horn = document.querySelector('#horn-select');
+  const audio = document.getElementsByClassName("hidden");
+  const imgs = document.getElementsByTagName("img");
+  horn.addEventListener('input', function() {
+    if (horn.value != "select") {
+      imgs[0].src = "assets/images/" + horn.value + ".svg";
+      audio.src = "assets/audio/" + horn.value + ".mp3";
+     }
+   });
+  
 }

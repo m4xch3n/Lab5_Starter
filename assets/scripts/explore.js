93ea6fc3fd88b52d78a001faa4aac4bd4b7548fb
Maximillian Chen <maxchensemail@gmail.com>
2022-10-28 14:47:16 -0700

    explore drop down menu population

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,17 @@
 // explore.js
 
 window.addEventListener('DOMContentLoaded', init);
+const synth = window.speechSynthesis;
 
 function init() {
-  // TODO
+  window.addEventListener('load', function() {
+    const voices = document.querySelector("#voice-select");
+    //console.log(voices);
+    let vox = synth.getVoices();
+    console.groupCollapsed(vox);
+    vox.forEach(element => {
+      voices.add(new Option(element.name));
+    });
+    console.log(voices);
+  });
 }

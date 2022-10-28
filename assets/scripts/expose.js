4d0cdc4327b992b839a229d3285b629c80647ee8
Maximillian Chen <maxchensemail@gmail.com>
2022-10-28 13:34:12 -0700

    Volume slider sets volume and picture

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -8,10 +8,33 @@ function init() {
   const audio = document.getElementsByClassName("hidden");
   const imgs = document.getElementsByTagName("img");
   horn.addEventListener('input', function() {
-    if (horn.value != "select") {
+    if(horn.value != "select") {
       imgs[0].src = "assets/images/" + horn.value + ".svg";
       audio.src = "assets/audio/" + horn.value + ".mp3";
      }
    });
+  const vol = document.getElementById('volume');
+  vol.addEventListener('change', function() {
+    if(vol.value == 0) {
+      imgs[1].src = "assets/icons/volume-level-0.svg";
+      audio.volume = 0.0;
+      //console.log(audio.volume);
+    }
+    else if(vol.value >= 1 && vol.value < 33) {
+      imgs[1].src = "assets/icons/volume-level-1.svg";
+      audio.volume = (vol.value / 100);
+      //console.log(audio.volume);
+    }
+    else if(vol.value >= 33 && vol.value < 67) {
+      imgs[1].src = "assets/icons/volume-level-2.svg";
+      audio.volume = (vol.value / 100);
+      //console.log(audio.volume);
+    }
+    else {
+      imgs[1].src = "assets/icons/volume-level-3.svg";
+      audio.volume = (vol.value / 100);
+      //console.log(audio.volume);
+    }
+  });
   
 }

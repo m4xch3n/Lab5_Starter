cf48929d23cf8ec43eb0c13127cff5f19fa02551
Maximillian Chen <maxchensemail@gmail.com>
2022-10-28 13:58:21 -0700

    Made Sound come out at right volume + confetti

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -10,31 +10,40 @@ function init() {
   horn.addEventListener('input', function() {
     if(horn.value != "select") {
       imgs[0].src = "assets/images/" + horn.value + ".svg";
-      audio.src = "assets/audio/" + horn.value + ".mp3";
+      audio[0].src = "assets/audio/" + horn.value + ".mp3";
+      //console.log(audio[0]);
+      //console.log(imgs[0]);
      }
    });
   const vol = document.getElementById('volume');
   vol.addEventListener('change', function() {
     if(vol.value == 0) {
       imgs[1].src = "assets/icons/volume-level-0.svg";
-      audio.volume = 0.0;
-      //console.log(audio.volume);
+      audio[0].volume = 0.0;
+      //console.log(audio[0].volume);
     }
     else if(vol.value >= 1 && vol.value < 33) {
       imgs[1].src = "assets/icons/volume-level-1.svg";
-      audio.volume = (vol.value / 100);
-      //console.log(audio.volume);
+      audio[0].volume = (vol.value / 100);
+      //console.log(audio[0].volume);
     }
     else if(vol.value >= 33 && vol.value < 67) {
       imgs[1].src = "assets/icons/volume-level-2.svg";
-      audio.volume = (vol.value / 100);
-      //console.log(audio.volume);
+      audio[0].volume = (vol.value / 100);
+      //console.log(audio[0].volume);
     }
     else {
       imgs[1].src = "assets/icons/volume-level-3.svg";
-      audio.volume = (vol.value / 100);
-      //console.log(audio.volume);
+      audio[0].volume = (vol.value / 100);
+      //console.log(audio[0].volume);
     }
   });
-  
+  const button = document.querySelector('button');
+  button.addEventListener('click', function() {
+    if(audio.value != "select") {
+      if(horn.value == "party-horn" && audio.value != 0)
+        jsConfetti.addConfetti();
+      audio[0].play();
+    }
+  });
 }

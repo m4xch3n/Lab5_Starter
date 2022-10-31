d69a6386385ce2cfd6e6eb032ac50494af0ea51d
Maximillian Chen <maxchensemail@gmail.com>
2022-10-30 22:37:01 -0700

    added links to README

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Lab 5 - Starter
 Maximillian Chen
+https://m4xch3n.github.io/Lab5_Starter/expose.html
+https://m4xch3n.github.io/Lab5_Starter/explore.html
--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -8,10 +8,16 @@ function init() {
     const voices = document.querySelector("#voice-select");
     //console.log(voices);
     let vox = synth.getVoices();
-    console.groupCollapsed(vox);
+    //console.groupCollapsed(vox);
     vox.forEach(element => {
       voices.add(new Option(element.name));
     });
-    console.log(voices);
+    //console.log(voices);
+  });
+  const button = document.querySelector('button');
+  const textarea = document.getElementById("text-to-speek");
+  console.log(button);
+  button.addEventListener('click', function() {
+    console.log(textarea.value);
   });
 }
